dc5bd161335ab7d6ab5eed0a3040ff1fec25bd8e
QwertygidQ <Qwertygid@gmail.com>
2018-05-12 17:31:46 +0300

    New object selection

--- app/static/js/checkboxes.js
+++ app/static/js/checkboxes.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {    
-    $('#osm_checkbox').checkbox({
-        onChecked: function() {console.log('onChecked osm');},
-        onUnchecked: function() {console.log('onUnchecked osm');}
+    $('#osm_checkbox1').checkbox({
+        onChecked: function() {console.log('onChecked osm1');},
+        onUnchecked: function() {console.log('onUnchecked osm1');}
     });
     
-    $('#dg_checkbox').checkbox({
-        onChecked: function() {console.log('onChecked dg');},
-        onUnchecked: function() {console.log('onUnchecked dg');}
+    $('#dg_checkbox1').checkbox({
+        onChecked: function() {console.log('onChecked dg1');},
+        onUnchecked: function() {console.log('onUnchecked dg1');}
     });
     
-    $('#osm_checkbox').checkbox('check');
-    $('#dg_checkbox').checkbox('check');
+    $('#osm_checkbox1').checkbox({
+        onChecked: function() {console.log('onChecked osm2');},
+        onUnchecked: function() {console.log('onUnchecked osm2');}
+    });
+    
+    $('#dg_checkbox1').checkbox({
+        onChecked: function() {console.log('onChecked dg2');},
+        onUnchecked: function() {console.log('onUnchecked dg2');}
+    });
+    
+    $('#osm_checkbox1').checkbox('check');
+    $('#dg_checkbox2').checkbox('check');
 });
