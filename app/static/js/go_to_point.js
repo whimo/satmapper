3a8cbe4df50b8cbc7e6813823d151a12130ca4d9
QwertygidQ <Qwertygid@gmail.com>
2018-05-12 16:07:37 +0300

    Go to point

---
+++ app/static/js/go_to_point.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){    
+    $("#go_button").click(function() {
+        let latitude = $('#latitude').val();
+        if (latitude === '' || isNaN(latitude))
+        {
+            alert('Latitude should be a valid number!');
+            return;
+        }
+        
+        let longitude = $('#longitude').val();
+        if (longitude === '' || isNaN(longitude))
+        {
+            alert('Longitude should be a valid number!');
+            return;
+        }
+        
+        let new_coords = [parseFloat(latitude), parseFloat(longitude)];
+        
+        osm_map.setView(new_coords);
+        l8_map.setView(new_coords);
+    });
+});
